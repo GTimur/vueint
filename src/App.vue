20bfe86cb33a00b9d1c1176afa94ada13cb3952f
GTimur <to-timur@yandex.ru>
2017-09-21 15:58:51 +0300

    v01

---
+++ src/App.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="app" class="wrapper">
+    <div class="header">
+      <div class="row inner">
+        <div class="col-md-1">
+        </div>
+        <div class="col-md-10">
+          <mainmenu></mainmenu>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="inner">
+        <div class="raw scrollable">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-10">
+            <div class="page myDiv">
+              <router-view></router-view>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="row inner">
+        <div class="col-md-1">
+        </div>
+        <div class="col-md-10 bg">
+          Nice Vuejs Application
+          <br> Version 1.0 (C) 2017
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+// import VueImg from 'v-img'
+// Vue.use(VueImg)
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import MainMenu from '@/components/MainMenu'
+import Hello from '@/components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    mainmenu: MainMenu,
+    hello: Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 3px;*/
+}
+
+.bg {
+  background-color: #A9A9A9;
+}
+
+.lpad {
+  padding-left: 10em;
+  padding-right: 10em;
+}
+
+.page {
+  display: block;
+  text-align: center;
+  font-size: 16px;
+  color: #eee;
+  border: 1px solid white;
+}
+
+.myDiv {
+  height: 100%;
+  max-height: calc(100vh - 50px);
+  width: 100%;
+  background-color: #FF0000;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+
+.wrapper {
+  height: 100%;
+  width: 100%;
+  display: table;
+}
+
+.header,
+.content,
+.footer {
+  display: table-row;
+}
+
+.header,
+.footer {
+  background: lightgrey;
+}
+
+.inner {
+  display: table-cell;
+}
+
+.content .inner {
+  height: 100%;
+  position: relative;
+  background: lightgrey;
+}
+
+.scrollable {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  overflow: auto;
+}
+</style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="mainmenu">       
+                <b-nav links class="bg roundborder">
+                    <b-nav-item>
+                        <router-link to="/">
+                        <span v-on:click="currentpath = 'Hello'">
+                        Главная</span>
+                        </router-link>
+                    </b-nav-item>
+                    <b-nav-item>
+                        <router-link to="/about">
+                        <span v-on:click="currentpath = 'About'">
+                        Вторая</span>
+                        </router-link>
+                    </b-nav-item>
+                    <b-nav-item>
+                        <router-link to="/folio">
+                        <span v-on:click="currentpath = 'Folio'">
+                        Третья</span>                        
+                        </router-link>
+                    </b-nav-item>
+                    <b-nav-item>
+                        <router-link to="/notes">
+                        <span v-on:click="currentpath = 'Notes'">
+                        Четвертая</span>                        
+                        </router-link>
+                    </b-nav-item>
+                    <b-nav-item>
+                        <router-link to="/download">
+                        <span v-on:click="currentpath = 'Download'">
+                        test
+                        </span>                        
+                        </router-link>
+                    </b-nav-item>
+
+                    <!--b-nav-item>
+                        <router-link to="">
+                        <span v-on:click="greet">
+                        Debuglink
+                        </span>
+                        </router-link>                        
+                    </b-nav-item>
+                    <b-nav-item disabled>
+                        <router-link to="">                        
+                        {{ currentpath }}
+                        </router-link>                        
+                    </b-nav-item-->
+                </b-nav>                    
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'mainmenu',
+  data: function () {
+    return {
+      currentpath: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+     color: #FFFFFF; /*#42b983;*/
+}
+
+.lpad {
+  padding-left:10em;
+  padding-right:10em;
+}
+
+.bg {
+  background-color: #A9A9A9;
+}
+
+.bg2 {
+  background-color: #FFFFF0;
+}
+
+.roundborder {
+-moz-border-radius:4px;
+-khtml-border-radius:4px;
+-webkit-border-radius:4px;
+border-radius:4px;
+/*border:1px solid white;*/
+}
+
+/* xs - устройства (до 576px) */
+/* CSS для ширины, которая меньше 575px (включительно) */
+ 
+/* sm-устройства (больше или равно 576px) */
+@media (min-width: 576px) {
+  /* CSS для: 576px <= ширины <= 767px */
+  .lpad {
+  padding-left:0em;
+  padding-right:0em;
+
+}
+}
+ 
+/* md-устройства (больше или равно 768px) */
+@media (min-width: 768px) {
+  /* CSS для: 768px <= ширины <= 991px */
+    .lpad {
+    padding-left:2em;
+  padding-right:2em;
+
+}
+}
+ 
+/* lg-устройства (больше или равно 992px) */
+@media (min-width: 992px) {
+  /* CSS для: 992px <= ширины <= 1119px */
+    .lpad {
+    padding-left:5em;
+  padding-right:5em;
+
+}
+}
+ 
+/* xl-устройства (больше или равно 1200px) */
+@media (min-width: 1200px) {
+  /* CSS для: ширины >= 1200px */
+    .lpad {
+    padding-left:10em;
+    padding-right:10em;
+
+}
+}
+
+</style>
